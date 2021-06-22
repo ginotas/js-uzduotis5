4a46b31ddf43231e77a7dbd2a995845a9b21613e
ginotas <ginotas@gmail.com>
2021-06-22 10:08:00 +0300

    vv

--- script.js
+++ script.js
@@ -135,3 +135,16 @@ document.getElementById("generavimas").addEventListener("click", function() {
     document.querySelector(".divas").append(divElementas);
     }
 });
+
+
+/*
+3. Susikurti objektą "Kvadratas".
+Objektas turi priimti du kintamuosius: a ir b kraštines.
+Sukurti du metodus, kurie skaičiuoja kvadrato plotą, perimetrą, įstrižainės ilgį.
+Informaciją išvesti į <p> žymę.
+
+
+Papildoma: a ir b kintamieji apibrežia kvadratą pikseliais. 
+Pagal įvestas reikšmes į input, HTML nubraižomas kvadrato objektas.  
+*/
+

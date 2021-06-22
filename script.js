87891c76c1c1a73ce5bd43b962168a6f925d62e2
ginotas <ginotas@gmail.com>
2021-06-22 09:49:27 +0300

    2 uzduotis

--- script.js
+++ script.js
@@ -105,9 +105,33 @@ document.getElementById("skaiciuoti").addEventListener("click", function() {
 * Objekto vidiniai kintamieji: id, vardas.
 * Vardas reikšmė = vardas1,vardas2 ir t.t.  
 * Id reikšmė = 1, 2 ir t.t.
+Papildomai:
+Pagal sukurtą obejktų masyvą, sukurti tiek pat div elementų, kuriuose išvedamas id ir vardas. Kas antras div - mėlynas.
 */
 
 document.getElementById("generavimas").addEventListener("click", function() {
 
+    let masyvas;
+    masyvas = [];
+    let objektas;
+    objektas = {};
+
+    for (let i = 0; i < 100; i++) {
+        objektas[i+1] = {
+            id: i+1,
+            vardas: "vardas"+[i+1],
+        }
+        masyvas[i] = objektas[i+1];
+        
+        console.log(masyvas[i]);
     
+
+    let divElementas = document.createElement("div"); 
+    divElementas.classList.add("div-dydis");
+    divElementas.innerText = "Vardas: " + masyvas[i].vardas + " " + "ID: " + masyvas[i].id; 
+    if( (i+1) % 2 == 0 ) {
+        divElementas.classList.add("spalva");
+    }
+    document.querySelector(".divas").append(divElementas);
+    }
 });

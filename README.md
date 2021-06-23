bfde5933fb2bedd1c5ba44130f11c5c2c419fa7e
ginotas <ginotas@gmail.com>
2021-06-23 09:02:38 +0300

    done

--- README.md
+++ README.md
@@ -19,12 +19,12 @@ Papildomai:
 Pagal sukurtą obejktų masyvą, sukurti tiek pat div elementų, kuriuose išvedamas id ir vardas. Kas antras div - mėlynas.
 
 
-3. Susikurti objektą "Kvadratas".
+ 3. Susikurti objektą "Kvadratas".
 Objektas turi priimti du kintamuosius: a ir b kraštines.
 Sukurti du metodus, kurie skaičiuoja kvadrato plotą, perimetrą, įstrižainės ilgį.
 Informaciją išvesti į <p> žymę.
 
 
-Papildoma: a ir b kintamieji apibrežia kvadratą pikseliais. Pagal įvestas reikšmes į input, HTML nubraižomas kvadrato objektas.      
+Papildoma: a ir b kintamieji apibrežia kvadratą pikseliais. Pagal įvestas reikšmes į input, HTML nubraižomas kvadrato objektas.     
 
 
--- script.js
+++ script.js
@@ -111,24 +111,19 @@ Pagal sukurtą obejktų masyvą, sukurti tiek pat div elementų, kuriuose išved
 
 document.getElementById("generavimas").addEventListener("click", function() {
 
-    let masyvas;
-    masyvas = [];
-    let objektas;
-    objektas = {};
+    let masyvas = [];
 
-    for (let i = 0; i < 100; i++) {
-        objektas[i+1] = {
+    for (let i=0; i<100; i++) {
+        let objektas = {
             id: i+1,
-            vardas: "vardas"+[i+1],
-        }
-        masyvas[i] = objektas[i+1];
-        
-        console.log(masyvas[i]);
+            vardas: "vardas"+(i+1)
+        };
+        masyvas[i] = objektas;  
     
 
     let divElementas = document.createElement("div"); 
     divElementas.classList.add("div-dydis");
-    divElementas.innerText = "Vardas: " + masyvas[i].vardas + " " + "ID: " + masyvas[i].id; 
+    divElementas.innerHTML = objektas.id + " " + objektas.vardas;
     if( (i+1) % 2 == 0 ) {
         divElementas.classList.add("spalva");
     }
@@ -148,3 +143,49 @@ Papildoma: a ir b kintamieji apibrežia kvadratą pikseliais.
 Pagal įvestas reikšmes į input, HTML nubraižomas kvadrato objektas.  
 */
 
+document.getElementById("nubrezk").addEventListener("click", function() {
+    let a, b;
+    a = parseFloat(document.getElementById("a-krastine").value);
+    a = parseFloat(document.getElementById("b-krastine").value);
+
+    let kvadratas = {
+        a: 300,
+        b: 400,
+    
+        paleidimas: function () {
+            a = a;
+            b = b;
+        },
+    
+        plotasPerimetras: function() {
+    
+            let plotas;
+            let perimetras;
+    
+            plotas = this.a * this.b;
+            perimetras = 2 * this.a + 2 * this.b;
+            return [plotas, perimetras]
+        },
+        istrizaine: function() {
+            let ilgis;
+            ilgis = Math.sqrt(this.a * this.a + this.b * this.b);
+            return ilgis;
+        },
+        nubrezk: function() {
+            document.querySelector(".keturkampis").style.width = this.a + "px";
+            document.querySelector(".keturkampis").style.height = this.b + "px";
+        },
+        isvesk: function() {
+            document.getElementById("perimetras").innerText = "kvadrato perimetras " + kvadratas.plotasPerimetras()[1];
+            document.getElementById("plotas").innerText = "kvadrato plotas " + kvadratas.plotasPerimetras()[0];
+            document.getElementById("istrizaine").innerText = "kvadrato istrizaine " + kvadratas.istrizaine();
+        },
+    };
+    
+    
+    
+    
+    kvadratas.paleidimas();
+    kvadratas.isvesk();
+    kvadratas.nubrezk();
+});
